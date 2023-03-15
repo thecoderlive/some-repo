181278a126bcb037e7dff53b8a7df2ad5750c3f6
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-15 12:47:02 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "83241892-a3a7-4105-9f19-0ff9e6930f93",
+    "home_feed": [
+        {
+            "id": "a1849cb3-349c-4366-877f-2b1ae05cef03",
+            "artist_name": "report",
+            "home_carousel": [
+                {
+                    "id": "7253fb04-c4b8-4839-b136-0ba694a913e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                },
+                {
+                    "id": "9dcb3012-5a62-447d-bd16-31acaf3f1a8c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                },
+                {
+                    "id": "d778146a-1840-4796-b375-9b2d2f5e4d23",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                },
+                {
+                    "id": "7df368ab-b924-4188-a210-56d6450261de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "2ee90320-6789-488a-9965-c4aada14ab7b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                }
+            ],
+            "art_caption": "like",
+            "art_description": "Change head together so. Family across turn science cold do."
+        },
+        {
+            "id": "4a882d36-4b48-47df-b7f3-be357b06751c",
+            "artist_name": "but",
+            "home_carousel": [
+                {
+                    "id": "50feccce-908c-450e-bde4-b7c75f7ea06c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "52b51cb3-1728-44db-90d2-02f81c96517d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "cce164aa-614d-47db-9253-ce49edf73735",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "89b97a1a-0688-497e-9a51-bbf62cb4426b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "c9e142df-39d2-490b-9b8c-388d6bdc7b7a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                }
+            ],
+            "art_caption": "energy",
+            "art_description": "News song analysis citizen. Song best may."
+        },
+        {
+            "id": "454f6d9c-9750-4dd6-8999-e7298fa58fe2",
+            "artist_name": "course",
+            "home_carousel": [
+                {
+                    "id": "4bb9f16d-7a3c-40b0-8f4f-cba00d1911ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                },
+                {
+                    "id": "9d4cd7b1-1352-418b-bfe5-55dd5b2759b8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png"
+                },
+                {
+                    "id": "80f97bec-4677-4fcd-ab68-74c06f5d83e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "1318bc40-c543-439c-8ceb-ed5d6e3e52df",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+                },
+                {
+                    "id": "6222f7a0-de77-46d4-99d7-bb4c986277c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                }
+            ],
+            "art_caption": "never",
+            "art_description": "Carry will sing commercial necessary if consider. Candidate marriage individual let draw experience including listen."
+        },
+        {
+            "id": "e9a3f6ab-a302-46cc-bcdd-21667b1fec6f",
+            "artist_name": "mother",
+            "home_carousel": [
+                {
+                    "id": "9dfc8923-994c-4044-be2a-80371462329b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "cca3fe1f-1e98-4d49-837e-a2d9e07a6897",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "f45cd6d9-6b4d-4855-bba4-74b4b991a7ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "40f03bf4-044f-4800-bf29-ca0dbd08c9de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "0d230d51-402a-48cb-aa0e-a552c095b8e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                }
+            ],
+            "art_caption": "quickly",
+            "art_description": "Water action visit can. City new prevent lay here keep gun. Cut customer say religious close."
+        },
+        {
+            "id": "a942edb9-5326-4c9f-8891-65f76f9fedcc",
+            "artist_name": "main",
+            "home_carousel": [
+                {
+                    "id": "7bd41b93-233f-4f0c-98d5-b0239727d7b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "01dc3231-630c-49d1-9932-4c2c543a44ea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png"
+                },
+                {
+                    "id": "95730e1e-816b-480d-b00e-0437ba16eb05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "d5b5cdb9-d5f7-48b6-ac71-334039b185fb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "ee36f1e8-424b-4e03-b848-8f2b39c510d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                }
+            ],
+            "art_caption": "recently",
+            "art_description": "Likely fast style building explain far out. South point deep customer agreement recognize. Actually dream possible."
+        }
+    ]
+}
